1dc4682c24339dd7033c6130b9e37d80dd01e29d
Amir <amir@amirrajabi.com>
2019-05-30 08:36:43 +1000

    First commit for Typescript version

---
+++ src/components/pages/app.tsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import Loadable from "react-loadable";
+import { Router, Link, RouteComponentProps } from "@reach/router";
+import { Global, css } from "@emotion/core";
+
+import Home from "./home";
+
+const LoadablePage2 = Loadable({
+  loader: () => import("./page2"),
+  loading() {
+    return <h1>Loading Split out code...</h1>;
+  }
+});
+
+class App extends Component<RouteComponentProps> {
+  public componentDidMount() {
+    console.log("Tsx did mount!");
+  }
+
+  public render() {
+    return (
+      <div className="App">
+        <Global
+          styles={css`
+            * {
+              padding: 10px;
+            }
+          `}
+        />
+        <h1>Hello my friend (Typescript Project)!</h1>
+        <Link to="/">Home</Link>
+        <Link to="/page2/1234">Page2</Link>
+        <Router>
+          <Home path="/" />
+          <LoadablePage2 path="/page2/:id" />
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
